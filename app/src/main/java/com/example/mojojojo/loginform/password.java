1e3850ee3616da7a897f85c920d1700f29544aa7
dhyanvi shah <dhyanvishah1998@gmail.com>
2019-02-18 17:03:15 +0530

    confirmpassword

--- app/src/main/java/com/example/mojojojo/loginform/password.java
+++ app/src/main/java/com/example/mojojojo/loginform/password.java
@@ -3,11 +3,13 @@ package com.example.mojojojo.loginform;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class password extends AppCompatActivity {
-    TextView t1;
-    TextView t2;
+    TextView tx1;
+    EditText t1;
+    EditText t2;
     Button b1;
 
 
@@ -16,9 +18,9 @@ public class password extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password);
-
-t1=(TextView)findViewById(R.id.change_pass);
-t1=(TextView)findViewById(R.id.confirm_pass);
+tx1=(TextView)findViewById(R.id.tx);
+t1=(EditText) findViewById(R.id.change_pass);
+t2=(EditText) findViewById(R.id.confirm_pass);
 b1=(Button)findViewById(R.id.submit_pass);
     }
 }

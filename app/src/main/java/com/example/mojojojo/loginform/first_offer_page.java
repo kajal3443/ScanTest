49e32c2adec46dde21cfe6539b8f6b92f9214a66
kajalshah3443 <shahkajal3443@gmail.com>
2019-02-19 10:55:29 +0530

    111

--- app/src/main/java/com/example/mojojojo/loginform/first_offer_page.java
+++ app/src/main/java/com/example/mojojojo/loginform/first_offer_page.java
@@ -9,5 +9,6 @@ public class first_offer_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_offer_page);
+
     }
 }
